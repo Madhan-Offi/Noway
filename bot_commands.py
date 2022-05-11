afcfa7874dfff7d580d9bc4772cb9c56d4754e3b
Madhan-Offi <102455806+Madhan-Offi@users.noreply.github.com>
2022-05-11 15:47:26 +0530

    Cmd

---
+++ bot_commands.py
@@ -0,0 +1,8 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.GpCommand = 'gplink'
+        self.DropCommand = 'droplink'
+        
+
+BotCommands = _BotCommands()
